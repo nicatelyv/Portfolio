38d65a1686bf675e0d8b15779e12a90381757271
Nicat Aliyev <116622367+nicatelyv@users.noreply.github.com>
2024-01-06 21:14:08 +0400

    add

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -47,7 +47,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                   py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
-                  <p>Created with &hearts; by Nijat Aliyev  </p>
+                  <p>Created by Nijat Aliyev</p>
                   <p>&copy; 2024</p>
                </Box>
             </Grid>
